80bd6a43068e78dd58a238976bad560fd0364606
CBE7F1F65 <CBE7F1F65@b6d913b0-95f4-11de-a4d2-b33cf26bcc48>
2009-08-31 09:33:00 +0000

    备份

---
+++ DES_GOBSTG/DES_GOBSTG/Core/InitDllLoader.cpp
@@ -0,0 +1,22 @@
+#include "MainDependency.h"
+#include "Export.h"
+
+#pragma init_seg(lib)
+
+static class InitDllLoader {
+public:
+	InitDllLoader() 
+	{
+		module = LoadLibraryA(DEFAULT_HGEDLLPATH);
+	}
+
+	~InitDllLoader() 
+	{
+		FreeLibrary(module);
+	}
+
+private:
+
+	HMODULE module;
+
+} _initDllLoader;
